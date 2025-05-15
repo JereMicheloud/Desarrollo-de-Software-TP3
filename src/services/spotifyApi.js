62a387e98f551b18e9ca0c1a63af7e7dd9ef6cc3
Jeremías Micheloud <jeremiche20@gmail.com>
2025-05-14 23:27:11 -0300

    LOGIN API

--- src/services/spotifyApi.js
+++ src/services/spotifyApi.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const API_URL = 'https://api.spotify.com/v1';
+const TOKEN_URL = 'https://accounts.spotify.com/api/token';
 const clientId = localStorage.getItem("CLIENT_ID");
 const clientSecret = localStorage.getItem("CLIENT_SECRET");
 
